4a75f39f52ad894f22e6536be8f3d5fb9c846efc
riccikoo <reivalaura@gmail.com>
2025-12-19 09:58:06 +0700

    update

---
+++ config.py
@@ -0,0 +1,8 @@
+BASE_URL = "http://localhost/module10/public"
+TOKEN_URL = f"{BASE_URL}/Api/access_token"
+API_V8 = f"{BASE_URL}/Api/V8/module"
+
+CLIENT_ID = "28a99247-3767-43ea-f308-692421e042c6"
+CLIENT_SECRET = "mod10-client"
+
+SECRET_KEY = "mod10-client"
